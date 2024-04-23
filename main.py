be56cba90346ed6640b49a608c388f3604431a53
Xavi Domínguez <xavi@fablabbcn.org>
2024-04-23 12:19:06 +0200

    board

---
+++ main.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+import altair as alt
+import plotly.express as px
+
+st.set_page_config(
+    page_title="STEAMBuilder Dashboard",
+    page_icon="🏂",
+    layout="wide",
+    initial_sidebar_state="expanded")
+
+alt.themes.enable("dark")
