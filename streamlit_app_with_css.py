1f34c70162a2dd85a166d76a235f91964a1879d2
Xavi Domínguez <xavi@fablabbcn.org>
2024-04-23 22:25:30 +0200

    Update streamlit_app_with_css.py

--- streamlit_app_with_css.py
+++ streamlit_app_with_css.py
@@ -123,7 +123,7 @@ with st.sidebar:
     
     year_list = list(df_reshaped.year.unique())[::-1]
     
-    selected_year = st.selectbox('Selecciona un/a alumne', student_list)
+    selected_year = st.selectbox('Selecciona un/a alumne', year_list)
     df_selected_year = df_reshaped[df_reshaped.year == selected_year]
     df_selected_year_sorted = df_selected_year.sort_values(by="population", ascending=False)
 
