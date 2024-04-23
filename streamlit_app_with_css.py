9c5f39d7bab39275acf31530114a39435b0c0e5c
Xavi Domínguez <xavi@fablabbcn.org>
2024-04-23 22:36:55 +0200

    Update streamlit_app_with_css.py

--- streamlit_app_with_css.py
+++ streamlit_app_with_css.py
@@ -25,7 +25,7 @@ cur.execute(sql)
 
 # Obtiene los resultados de la consulta
 results_database = cur.fetchall()
-count = len(results_database)
+count_interactions = len(results_database)
 # Student List
 all_idc = []
 all_pregunta = []
@@ -236,29 +236,11 @@ col = st.columns((1.5, 4.5, 2), gap='medium')
 with col[0]:
     st.markdown('#### Dades Generals')
 
-    df_population_difference_sorted = calculate_population_difference(df_reshaped, selected_year)
+    df_num_interactions = count_interactions
 
-    if selected_year > 2010:
-        first_state_name = df_population_difference_sorted.states.iloc[0]
-        first_state_population = format_number(df_population_difference_sorted.population.iloc[0])
-        first_state_delta = format_number(df_population_difference_sorted.population_difference.iloc[0])
-    else:
-        first_state_name = '-'
-        first_state_population = '-'
-        first_state_delta = ''
-    st.metric(label=first_state_name, value=first_state_population, delta=first_state_delta)
+    st.metric(label='#Interaccions', value=df_num_interactions, delta='last_state_delta')
 
-    if selected_year > 2010:
-        last_state_name = df_population_difference_sorted.states.iloc[-1]
-        last_state_population = format_number(df_population_difference_sorted.population.iloc[-1])   
-        last_state_delta = format_number(df_population_difference_sorted.population_difference.iloc[-1])   
-    else:
-        last_state_name = '-'
-        last_state_population = '-'
-        last_state_delta = ''
-    st.metric(label=last_state_name, value=last_state_population, delta=last_state_delta)
-
-    
+    '''
     st.markdown('#### States Migration')
 
     if selected_year > 2010:
@@ -294,8 +276,10 @@ with col[1]:
     heatmap = make_heatmap(df_reshaped, 'year', 'states', 'population', selected_color_theme)
     st.altair_chart(heatmap, use_container_width=True)
     
+'''
 
 with col[2]:
+    '''
     st.markdown('#### Top States')
 
     st.dataframe(df_selected_year_sorted,
@@ -313,10 +297,8 @@ with col[2]:
                         max_value=max(df_selected_year_sorted.population),
                      )}
                  )
-    
-    with st.expander('About', expanded=True):
+    '''
+    with st.expander('Sobre...', expanded=True):
         st.write('''
-            - Data: [U.S. Census Bureau](https://www.census.gov/data/datasets/time-series/demo/popest/2010s-state-total.html).
-            - :orange[**Gains/Losses**]: states with high inbound/ outbound migration for selected year
-            - :orange[**States Migration**]: percentage of states with annual inbound/ outbound migration > 50,000
+            - Dades generals de STEAMBuilder
             ''')
